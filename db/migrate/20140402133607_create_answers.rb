408d23128e41994796011e6de014c669d944c196
Raphael Fargeon <raphaelfargeon@gmail.com>
2014-04-02 17:55:21 +0200

    tt

--- db/migrate/20140402133607_create_answers.rb
+++ db/migrate/20140402133607_create_answers.rb
@@ -3,7 +3,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.string :reponse
-	  t.integer :id, :null => false
+	  t.integer :id_answer, :null => false
       t.timestamps
     end
   end
